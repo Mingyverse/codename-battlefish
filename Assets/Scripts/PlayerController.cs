41095f828b8f5b39db289a95f359c3e41cdc038a
Mingyverse <teoh.yongming456@gmail.com>
2024-09-20 02:39:08 +0800

    diver anim

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private float movementSpeed = 3;
+    private Vector2 movement;
+    private Animator animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement = new Vector2(Input.GetAxis("Horizontal"), 0).normalized;
+        animator.SetFloat("Direction", Mathf.Abs(movement.magnitude * movementSpeed));
+
+        bool flipped = movement.x < 0;
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flipped ? 180f : 0f, 0f));
+    }
+
+    void FixedUpdate()
+    {
+        if (movement != Vector2.zero)
+        {
+            var xMovement = movement.x * movementSpeed * Time.deltaTime;
+            this.transform.Translate(new Vector3(xMovement, 0), Space.World);
+        }
+    }
+}
