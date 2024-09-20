c72f24813d04694c6f2737026f2ce308c3eeffb0
Mingyverse <teoh.yongming456@gmail.com>
2024-09-21 00:13:43 +0800

    Diver movement done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,7 +7,6 @@ public class PlayerController : MonoBehaviour
 
     [SerializeField] private float movementSpeed = 4;
     private Vector2 movement;
-    // private Vector2 movementY;
     private Animator animator;
     
     // Start is called before the first frame update
@@ -19,30 +18,47 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        movement = new Vector2(Input.GetAxis("Horizontal"), 0).normalized;
-        animator.SetFloat("Direction", Mathf.Abs(movement.magnitude * movementSpeed));
-
-        /* movementY = new Vector2(Input.GetAxis("Vertical"), 0).normalized; */
-
-        bool flippedX = movement.x < 0;
-        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flippedX ? 180f : 0f, 0f));
+        movement = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical")).normalized;
+        animator.SetFloat("Horizontal", Mathf.Abs(movement.x * movementSpeed));
+        animator.SetFloat("Vertical", Mathf.Abs(movement.y * movementSpeed));
         
-        /* bool flippedY = movementY.y < 0;
-        this.transform.rotation = Quaternion.Euler(new Vector3(0f, flippedY ? 180f : 0f, 0f)); */
+        // Calculate angle
+        float angleY = 0f;
+        float angleZ = 0f;
+
+        if (movement.x < 0)
+        {
+            angleY = 180f;
+        }
+        else if (movement.x > 0)
+        {
+            angleY = 0f;
+        }
+        
+        if (movement.y < 0 && movement.x != 0)
+        {
+            angleZ = -15f;
+        }
+        else if (movement.y > 0 && movement.x != 0)
+        {
+            angleZ = 15f;
+        }
+        
+        this.transform.rotation = Quaternion.Euler(new Vector3(0f, angleY, angleZ));
     }
 
     void FixedUpdate()
     {
-        if (movement != Vector2.zero)
+        if (movement.x != 0)
         {
             var xMovement = movement.x * movementSpeed * Time.deltaTime;
             this.transform.Translate(new Vector3(xMovement, 0), Space.World);
         }
         
-        /* if (movementY != Vector2.zero)
+        if (movement.y != 0)
         {
-            var yMovement = movementY.y * movementSpeed * Time.deltaTime;
-            this.transform.Translate(new Vector3(yMovement, 0), Space.World);
-        } */
+            var yMovement = movement.y * movementSpeed * Time.deltaTime;
+            this.transform.Translate(new Vector3(0, yMovement), Space.World);
+        }
     }
 }
