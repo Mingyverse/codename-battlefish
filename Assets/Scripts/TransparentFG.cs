00f9999ad11534db428f92014a48f6424e13b11b
Mingyverse <teoh.yongming456@gmail.com>
2024-09-21 03:58:01 +0800

    transparent foreground script

---
+++ Assets/Scripts/TransparentFG.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TransparentFG : MonoBehaviour
+{
+
+    public SpriteRenderer? foreground;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreground = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    IEnumerator FadeOutSprite(float duration, float startOpacity, float targetOpacity)
+    {
+        float opacity;
+        var time = 0f;
+        while (time < duration)
+        {
+            opacity = Mathf.Lerp(startOpacity, targetOpacity, time / duration);
+            foreground.color = new Color(1f, 1f, 1f, opacity);
+            time += Time.deltaTime;
+            yield return null;
+        }
+
+        opacity = targetOpacity;
+        foreground.color = new Color(1f, 1f, 1f, opacity);
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(FadeOutSprite(0.7f, 1.0f, 0.7f));
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            StartCoroutine(FadeOutSprite(0.7f, 0.7f, 1.0f));
+        }
+    }
+}
