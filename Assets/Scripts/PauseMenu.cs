809c0d3b86fc522d61a66fde98781b6113fd200f
Mingyverse <teoh.yongming456@gmail.com>
2024-09-24 22:20:28 +0800

    temp pause menu in diving scene

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -7,12 +7,12 @@ public class PauseMenu : MonoBehaviour
 {
 
     public GameObject? pauseMenu;
-    public bool isPaused;
+    public static bool isPaused;
     
     // Start is called before the first frame update
     void Start()
     {
-        pauseMenu.SetActive(false);
+        
     }
 
     // Update is called once per frame
