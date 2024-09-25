71a570f76c688e1a9d9ae638b4af307a37d9d244
Poleric <nixonleongkb@gmail.com>
2024-09-26 00:21:08 +0800

    redo initialization

--- Assets/Scripts/Fish/BattleFish.cs
+++ Assets/Scripts/Fish/BattleFish.cs
@@ -1,15 +1,32 @@
 using System;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class BattleFish : MonoBehaviour
 {
     public BattleFishBase battleFishBase = default!;
     [NonSerialized] public FishStats stats = default!;
     [NonSerialized] public Health health = default!;
-    [NonSerialized] public Level level;
-
-    private void Start()
+    [NonSerialized] public Level level = default!;
+    [NonSerialized] public FishAI ai = default!;
+    [NonSerialized] public FishAbility ability = default!;
+    
+    private void Awake()
     {
-        health = GetComponentInChildren<Health>();
+        Assert.IsNotNull(battleFishBase);
+        
+        level = new Level(this);   // TODO: save in persistence
+        
+        stats = FishStats.FromFish(this);  // initialize after level, depends on level
+        
+        health = GetComponent<Health>();  // initialize after stats, depends on stats
+        Assert.IsNotNull(health);
+        
+        ability = GetComponent<FishAbility>();
+        Assert.IsNotNull(ability);
+        
+        ai = GetComponent<FishAI>();
+        Assert.IsNotNull(ai);
+        
     }
 }
--- Assets/Scripts/Fish/BattleFishBase.cs
+++ Assets/Scripts/Fish/BattleFishBase.cs
@@ -3,14 +3,12 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "NewBattleFish", menuName = "Codename BattleFish/BattleFish Data", order = 0)]
 public class BattleFishBase : ScriptableObject
 {
-    public string fishName = default!;
-    public string description = default!;
+    public string fishName = "";
+    public string description = "";
     public FishRarity rarity;
     public WaterType waterType;
-    public FishStats.Stats baseStats = default!;
-    public FishStats.Stats levelUpStats = default!;
+    public FishStats.Stats baseStats = FishStats.Stats.Ones();
+    public FishStats.Stats levelUpStats = FishStats.Stats.Ones();
     public FishClass fishClass;
     public XpScaling xpScaling;
-    public FishAbility ability = default!;
-    public FishAI ai = default!;
 }
