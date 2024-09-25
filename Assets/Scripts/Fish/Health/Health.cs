8723b5c3002c82da17aa90a2dd8a7a291a72bcdc
Poleric <nixonleongkb@gmail.com>
2024-09-25 17:27:19 +0800

    add passive healing

--- Assets/Scripts/Fish/Health/Health.cs
+++ Assets/Scripts/Fish/Health/Health.cs
@@ -1,25 +1,56 @@
 using System;
+using System.Collections;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class Health : MonoBehaviour
 {
-    public float value { get; private set; }
+    public float value;
+    public float timeToPassiveHeal = 5;
+    public float passiveHealPerSecond = 1;
+    
     public float maxHp => _fish.stats.maxHp;
     public float percentage => value / maxHp;
+
+    private float _lastDamage;
+    private bool _isPassiveHealing;
+    private BattleFish _fish = default!;
     
     public delegate void HealthChangeEvent(Health health, float amount);
+
     public event HealthChangeEvent? OnHealthGain;
     public event HealthChangeEvent? OnHealthLost;
     public event HealthChangeEvent? OnHealthDeath;
-    
-    private BattleFish _fish = default!;
 
-    private void Start()
-    {
-        _fish = GetComponentInParent<BattleFish>();
+    private void Awake() {
+        _fish = GetComponent<BattleFish>();
+        Assert.IsNotNull(_fish);
+        
         value = maxHp;
     }
 
+    private void Update()
+    {
+        if (CanPassiveHeal() && !_isPassiveHealing)
+            StartCoroutine(StartPassiveHeal());
+    }
+
+    private IEnumerator StartPassiveHeal()
+    {
+        _isPassiveHealing = true;
+        while (CanPassiveHeal())
+        {
+            Heal(passiveHealPerSecond * Time.deltaTime);
+            yield return null;
+        }
+        _isPassiveHealing = false;
+    }
+
+    private bool CanPassiveHeal()
+    {
+        return value < maxHp && _lastDamage + timeToPassiveHeal < Time.time;
+    }
+    
     public void TakeDamage(float amount)
     {
         value -= amount;
@@ -28,6 +59,8 @@ public class Health : MonoBehaviour
         
         if (value <= 0)
             OnHealthDeath?.Invoke(this, amount);
+        
+        _lastDamage = Time.time;
     }
 
     public void Heal(float amount)
