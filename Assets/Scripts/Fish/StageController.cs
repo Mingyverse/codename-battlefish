66ba36057ed78de7335ca1aac7b70e8fa23d75d4
Poleric <nixonleongkb@gmail.com>
2024-09-26 00:20:18 +0800

    Stage controller for tracking fishes

---
+++ Assets/Scripts/Fish/StageController.cs
@@ -0,0 +1,49 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class StageController : MonoBehaviour
+{
+    [NonSerialized] public GameObject? player; 
+    [NonSerialized] public BattleFish[] fishes = Array.Empty<BattleFish>();
+    
+    public static StageController instance = default!;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    private void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+        fishes = FindObjectsOfType<BattleFish>();
+    }
+    
+    public Transform? GetClosestFish(Transform position, Predicate<BattleFish> predicate)
+    {
+        Transform? bestTarget = null;
+        float closestDistanceSqr = Mathf.Infinity;
+        Vector3 currentPosition = transform.position;
+        
+        foreach(BattleFish fish in fishes)
+        {
+            if (!predicate(fish))
+                continue;
+                
+            // calc distance
+            Transform target = fish.transform;
+            Vector3 directionToTarget = target.position - currentPosition;
+            float distanceSqrToTarget = directionToTarget.sqrMagnitude;
+            
+            if (distanceSqrToTarget < closestDistanceSqr)
+            {
+                closestDistanceSqr = distanceSqrToTarget;
+                bestTarget = target;
+            }
+        }
+     
+        return bestTarget;
+    }
+}
