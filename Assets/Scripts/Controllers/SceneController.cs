35240719ae3c310c0323d8f0c91cc9f8010606be
Poleric <nixonleongkb@gmail.com>
2024-09-27 02:05:30 +0800

    restructure

--- Assets/Scripts/Controllers/SceneController.cs
+++ Assets/Scripts/Controllers/SceneController.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class SwitchScene : MonoBehaviour
+public class SceneController : MonoBehaviour
 {
 
     public string? nextScene;
