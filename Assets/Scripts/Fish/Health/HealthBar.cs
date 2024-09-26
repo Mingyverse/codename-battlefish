6fd1481da6cb256de6027cdac5af49e100ce7466
Poleric <nixonleongkb@gmail.com>
2024-09-27 01:00:25 +0800

    add healthbar

--- Assets/Scripts/Fish/Health/HealthBar.cs
+++ Assets/Scripts/Fish/Health/HealthBar.cs
@@ -4,36 +4,61 @@ using UnityEngine.Assertions;
 
 public class HealthBar : MonoBehaviour
 {
-    public SpriteRenderer sprite = default!;
+    public SpriteRenderer healthSprite = default!;
+    public FadeTo[] spriteFaders = Array.Empty<FadeTo>();
     
     private BattleFish _fish = default!;
-
+    
     private void Awake()
     {
         _fish = GetComponentInParent<BattleFish>();
         Assert.IsNotNull(_fish);
         
-        Assert.IsNotNull(sprite);
+        Assert.IsNotNull(healthSprite);
+
+        if (spriteFaders.Length == 0)
+            spriteFaders = GetComponentsInChildren<FadeTo>();
+        Assert.IsFalse(spriteFaders.Length == 0);
     }
 
     private void Start()
     {
-        _fish = GetComponentInParent<BattleFish>();
+        foreach (FadeTo fadeTo in spriteFaders)
+            fadeTo.opacity = 0;
+                
         _fish.health.OnHealthGain += OnHealthChange;
+        _fish.health.OnHealthGain += FadeHealthBar;
         _fish.health.OnHealthLost += OnHealthChange;
+        _fish.health.OnHealthLost += AppearHealthBar;
     }
 
     private void OnHealthChange(Health health, float value)
     {
-        Vector3 scale = sprite.transform.localScale;
+        Vector3 scale = healthSprite.transform.localScale;
         scale.x = health.percentage;
         
-        sprite.transform.localScale = scale;
+        healthSprite.transform.localScale = scale;
+    }
+
+    private void FadeHealthBar(Health health, float value)
+    {
+        if (health.percentage >= 0.99f && spriteFaders[0].opacity > 0.99f)
+            foreach (FadeTo fadeTo in spriteFaders)
+                fadeTo.FadeIfNotAlready(0);
+    }
+
+    private void AppearHealthBar(Health health, float value)
+    {
+        if (spriteFaders[0].opacity < 0.001f)
+            foreach (FadeTo fadeTo in spriteFaders)
+                fadeTo.FadeIfNotAlready(100);
     }
 
     private void OnDestroy()
     {
         _fish.health.OnHealthGain -= OnHealthChange;
+        _fish.health.OnHealthGain -= FadeHealthBar;
         _fish.health.OnHealthLost -= OnHealthChange;
+        _fish.health.OnHealthLost -= AppearHealthBar;
     }
 }
