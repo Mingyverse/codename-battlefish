e9050245485a00ac35339df046fa80d9f1dd60e0
Poleric <nixonleongkb@gmail.com>
2024-09-28 02:00:14 +0800

    persistent playerdatacontroller

--- Assets/Scripts/Controllers/PlayerDataController.cs
+++ Assets/Scripts/Controllers/PlayerDataController.cs
@@ -1,6 +1,48 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 
+[Serializable]
 public class PlayerDataController : MonoBehaviour
 {
+    public struct FishData
+    {
+        public string fishId;
+        public int exp;
+    }
     
+    [NonSerialized] public static PlayerDataController instance = default!;
+    
+    public List<string> caughtFishesId = new List<string>();
+    public List<FishData> fishDatas = new List<FishData>(); 
+    public Dictionary<string, int> items = new Dictionary<string, int>();  
+    
+    private void Awake()
+    {
+        instance = this;
+        Load();
+        DontDestroyOnLoad(this);
+    }
+
+    public void Save()
+    {
+        string jsonSaveData = JsonUtility.ToJson(this);
+        string filepath = Application.persistentDataPath + "/playerData.json";
+        File.WriteAllText(filepath, jsonSaveData);
+    }
+
+    public void Load()
+    {
+        string filepath = Application.persistentDataPath + "/playerData.json";
+        if (!File.Exists(filepath)) {
+            return;
+        }
+        string text = File.ReadAllText(filepath);
+        PlayerDataController playerData = JsonUtility.FromJson<PlayerDataController>(text);
+        caughtFishesId = playerData.caughtFishesId;
+        fishDatas = playerData.fishDatas;
+        items = playerData.items;
+    }
 }
