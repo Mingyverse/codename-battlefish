133b3abcd8227c8ee7a4e47af4dae08d9bf8942e
Poleric <nixonleongkb@gmail.com>
2024-09-29 22:21:24 +0800

    rework logic and keep progress

--- Assets/Scripts/UI/WinLoseMenu.cs
+++ Assets/Scripts/UI/WinLoseMenu.cs
@@ -1,43 +1,65 @@
-using System;
 using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Assertions;
 
 public class WinLoseMenu : MonoBehaviour
 {
 
-    public GameObject? winUI;
-    public GameObject? loseUI;
-    public BattleFish[] ownFish;
-    public BattleFish[] enemyFish;
+    public GameObject winUI = default!;
+    public GameObject loseUI = default!;
 
-    // Start is called before the first frame update
-    void Start()
+    public string playerTeamTag = "Team1";
+    public string enemyTeamTag = "Team2";
+
+    private int _playerFishCount;
+    private int _enemyFishCount;
+
+    private void Awake()
+    {
+        Assert.IsNotNull(winUI);
+        Assert.IsNotNull(loseUI);
+    }
+
+    private void Start()
     {
         Time.timeScale = 1f;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        checkFishDead();
-    }
-
-    void checkFishDead()
-    {
-        for (int i = 0; i < ownFish.Length; i++)
+        foreach (var fish in FindObjectsByType<BattleFish>(FindObjectsSortMode.None))
         {
-            if (AllFalse(ownFish))
-            {
-                StartCoroutine(Wait(loseUI));
-            }
-            else if (AllFalse(enemyFish))
-            {
-                StartCoroutine(Wait(winUI));
-            }
+            TallyFish(fish);
+            fish.onDeath += OnFishDead;
         }
+    }
+
+    public void TallyFish(BattleFish fish)
+    {
+        if (fish.CompareTag(playerTeamTag))
+            _playerFishCount++;
+        else if (fish.CompareTag(enemyTeamTag))
+            _enemyFishCount++;
+    }
+
+    public void OnFishDead(BattleFish fish, BattleFish attacker)
+    {
+        if (fish.CompareTag(playerTeamTag))
+            _playerFishCount--;
+        else if (fish.CompareTag(enemyTeamTag))
+            _enemyFishCount--;
+        
+        CheckWin();
+        fish.onDeath -= OnFishDead;
+    }
+
+    public void CheckWin()
+    {
+        if (_playerFishCount == 0)
+            StartCoroutine(Wait(loseUI));
+        else if (_enemyFishCount == 0)
+        {
+            StartCoroutine(Wait(winUI));
+            PlayerDataController.instance.completedBattles.Add(StageController.instance.stageData.id);
+        }
+        
     }
 
     IEnumerator Wait(GameObject scene)
@@ -45,14 +67,5 @@ public class WinLoseMenu : MonoBehaviour
         yield return new WaitForSeconds(1.25f);
         scene.SetActive(true);
         Time.timeScale = 0f;
-    }
-
-    private bool AllFalse(BattleFish[] fishes)
-    {
-        for (int i = 0; i < fishes.Length; i++)
-        {
-            if (!fishes[i].health.isDead) return false;
-        }
-        return true;
     }
 }
